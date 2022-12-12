b19d277f742a653a0ab0c4bc18eeffd247132f6e
tess-4aek <ruslan.vyshnevskyi@nitrix.io>
2022-12-12 13:02:51 +0300

    add image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import img from './medconsult.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={img} alt="" style={{width: '100%', height: '100%'}} />
     </div>
   );
 }
